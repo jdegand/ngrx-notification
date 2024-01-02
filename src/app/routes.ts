0ddd0cf6ec7078c987870362533f272eeea9872d
jdegand <70610011+jdegand@users.noreply.github.com>
2024-01-02 15:01:10 -0500

    fix: routes loading

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,7 +1,26 @@
 import { Route } from '@angular/router';
+import { TeacherComponent } from './teacher/teacher.component';
 
+export const ROUTES: Route[] = [
+  { path: '', pathMatch: 'full', redirectTo: 'teacher' },
+  {
+    path: 'teacher',
+    component: TeacherComponent,
+  },
+  {
+    path: 'student',
+    loadComponent: () =>
+      import('./student/student.component').then((m) => m.StudentComponent),
+  },
+  {
+    path: 'school',
+    loadComponent: () =>
+      import('./school/school.component').then((m) => m.SchoolComponent),
+  },
+];
+
+/*
 // shouldn't lazy load the home route
-
 export const ROUTES: Route[] = [
   { path: '', pathMatch: 'full', redirectTo: 'teacher' },
   {
@@ -20,3 +39,4 @@ export const ROUTES: Route[] = [
       import('./school/school.component').then((m) => m.SchoolComponent),
   },
 ];
+*/
