a1c05db268cfd48211680275b781fb86d455d45a
jdegand <70610011+jdegand@users.noreply.github.com>
2024-01-06 15:19:34 -0500

    docs: createFeature parallel move

--- src/app/school/store/school.reducer.ts
+++ src/app/school/store/school.reducer.ts
@@ -22,3 +22,42 @@ export const schoolReducer = createReducer(
 
 export const { selectIds, selectEntities, selectAll, selectTotal } =
     SchoolAdapter.getSelectors();
+
+/*
+
+import { School } from '../../../model/school.model';
+import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
+import { createFeature, createReducer, on } from '@ngrx/store';
+import { schoolActions } from './school.actions';
+
+export type SchoolState = EntityState<School>;
+
+export const SchoolAdapter: EntityAdapter<School> =
+    createEntityAdapter<School>();
+
+const schoolsFeature = createFeature({
+    name: 'Schools',
+    reducer: createReducer(
+          SchoolAdapter.getInitialState(),
+          on(schoolActions.addOneSchool, (state, { school }) =>
+              SchoolAdapter.upsertOne(school, state)
+          ),
+          on(schoolActions.addAllSchools, (state, { schools }) =>
+              SchoolAdapter.setAll(schools, state)
+          )
+      )
+  });
+  
+  export const {
+    name: schoolsFeatureKey,
+    reducer: schoolReducer,
+    selectIds, 
+    selectEntities,
+  } = schoolsFeature;
+  
+  // could move the rest of school.selector code into this file
+  
+  // this app uses EntityAdapter to handle the collections  
+  // you still have to create and export selectors
+
+*/
